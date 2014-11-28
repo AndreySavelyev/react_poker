2c15d83b532d3505e7786f40bf1e038401ae8b77
Maksim Sitnikov <sitnikovme@gmail.com>
2014-11-28 16:41:29 +0300

    second

--- src/scripts/components/LoginPage.jsx
+++ src/scripts/components/LoginPage.jsx
@@ -9,10 +9,26 @@ var Link = Routes.Link;
 var React = require('react/addons');
 
 var LoginPage = React.createClass({
+  getInitialState: function() {
+      return {username: 'user'};
+  },
+  // componentDidMount: function() {
+  //   this.username = 'user';
+  // },
+  handleChange: function(event) {
+    this.setState({username: event.target.value});
+    console.log(event);
+  },
+  handleClick: function() {
+  },
   render: function() {
     return (
       <div className='main'>
         <Link to="room">Room</Link>
+        <div>Test6 </div>
+        <div>{this.state.username}</div>
+        <input type="text" value={this.state.username} onChange={this.handleChange}/>
+        <button onClick={this.handleClick}>Login</button>
       </div>
     );
   }
--- src/scripts/components/RoomPage.jsx
+++ src/scripts/components/RoomPage.jsx
@@ -5,15 +5,58 @@
 'use strict';
 
 var React = require('react/addons');
+var _ = require('lodash')
 
 var Routes = require('react-router');
 var Link = Routes.Link;
 
+var MessageList = require('./MessageList');
+var UserList = require('./UserList');
+var SendForm = require('./SendForm');
+var moment = require('moment')
+
 var RoomPage = React.createClass({
+  addMessage: function(user, text){
+    console.log(user);
+    console.log(text);
+    var messages = this.state.messages
+    var m = moment();
+    messages.push({
+      id: m.unix(),
+      text: text,
+      user: user,
+      timestamp: m
+    })
+    this.setState({messages: messages});
+  },
+  getInitialState: function () {
+    return {
+      currentUser: {
+        name: 'username',
+      },
+      userlist: [{name: 'username'}, {name: 'username2'}, {name: 'username3'}],
+      messages: [
+        {
+          id: 1,
+          user: 'username 1',
+          text: 'Message1',
+          timestamp: moment()
+        },
+        {
+          id: 2,
+          user: 'username2',
+          text: 'Message2',
+          timestamp: moment()
+        }
+      ]
+    };
+  },
   render: function() {
     return (
-      <div className='main'>
-        <Link to="login">Back</Link>
+      <div>
+        <MessageList messages={this.state.messages} />
+        <UserList userlist={this.state.userlist} />
+        <SendForm user={this.state.currentUser} onsubmit={this.addMessage}/>
       </div>
     );
   }
